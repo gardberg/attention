fab7862f83c10aa6ee6ff14aaf58db5f8431cea1
LukasGardberg <lukasgardberg2@gmail.com>
2024-01-16 22:44:09 +0100

    added tokenizer

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,9 @@
 import logging
+import os
+import jax
+
+os.environ["TIKTOKEN_CACHE_DIR"] = "../.cache"
+import tiktoken
 
 LOG_LEVEL = 25
 # LOG_LEVEL = logging.DEBUG
@@ -20,6 +25,24 @@ def get_logger():
     logger.addHandler(ch)
 
     return logger
+
+    
+class Tokenizer:
+    # Wrapper class for tiktoken tokenizer
+    
+    def __init__(self, name='cl100k_base'):
+        try:
+            self.encoding = tiktoken.get_encoding(name)
+        except:
+            raise Exception(f"Could not download or find tiktoken tokenizer: '{name}'")
+
+    def encode(self, text: str) -> jax.Array:
+        enc_list = self.encoding.encode(text)
+        return jax.numpy.array(enc_list)
+
+    # TODO: How do we handle batched array?
+    def decode(self, enc: jax.Array) -> str:
+        return self.encoding.decode(enc.tolist())
 
 
 if __name__ == "__main__":
